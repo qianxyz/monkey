b19aca5d9becff1a5d1936e4c497158ea7a69c3d
Qianxy <xyqian67@gmail.com>
2022-11-25 17:49:59 +0800

    Initialize dev branch

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-mod ast;
-mod lexer;
-mod parser;
-mod repl;
-mod token;
+//mod ast;
+//mod lexer;
+//mod parser;
+//mod repl;
+//mod token;
 
+/// Welcome to the dev branch!
+/// Currently this branch attempts to simplify code.
+/// At the moment, there is huge redundancy in the representation
+/// of tokens and AST. Hopefully we can come up with cleaner structures
+/// so that writing tests will be easier.
 fn main() {
-    repl::run();
+    println!("hello monkey");
 }
