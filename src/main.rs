cc1652f5847800934f40a700febaa0d05474a9dd
Qianxy <xyqian67@gmail.com>
2022-11-27 02:22:57 +0800

    Remove comments

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,6 @@ mod parser;
 mod repl;
 mod token;
 
-/// Welcome to the dev branch!
-/// Currently this branch attempts to simplify code.
-/// At the moment, there is huge redundancy in the representation
-/// of tokens and AST. Hopefully we can come up with cleaner structures
-/// so that writing tests will be easier.
 fn main() {
     repl::run();
 }
